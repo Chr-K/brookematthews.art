691fbb6c242bdb82c9d3884a1b0167763c1983df
Christopher Kufis <christopherkufis@icloud.com>
2023-04-25 16:07:09 -0700

    test

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,9 @@
 <?php
-
-use Controllers\ItemController;
-
 header('Access-Control-Allow-Origin: https://brookematthews.art');
 require __DIR__ . '/vendor/autoload.php';
 require_once './router.php';
 session_start();
-
+use Controllers\ItemController;
 $router = new Router();
 $router->addRoute('GET','/getitems','ItemController','get_items');
 $router->handleRequest();
