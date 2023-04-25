6ea33b2c5ef4f5a5428739b95931c99e52eb649b
Christopher Kufis <christopherkufis@icloud.com>
2023-04-25 15:13:25 -0700

    model implementation

--- backend/controllers/items_controller.php
+++ backend/controllers/items_controller.php
@@ -1,8 +1,12 @@
 <?php
 namespace controller;
+
+use model\item_model;
 class items_controller{
     function get_items(){
-        return ('items here');
+        $model = new item_model;
+        $data = $model->get_items();
+        return($data);
     }
 }
 
--- backend/models/items_model.php
+++ backend/models/items_model.php
@@ -1,7 +1,9 @@
 <?php
+namespace model;
+
 class item_model{
     function get_items(){
-        
+        return('error 5. more coffee required');
     }
 }
 
