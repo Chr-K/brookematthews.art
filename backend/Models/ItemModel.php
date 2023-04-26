4ff45da2908731c4c27e6511a560149d62bdacb8
Christopher Kufis <christopherkufis@icloud.com>
2023-04-26 06:04:40 -0700

    fixed

--- backend/Models/ItemModel.php
+++ backend/Models/ItemModel.php
@@ -3,8 +3,8 @@ namespace Models;
 
 class ItemModel{
     function get_items(){
-        /*
-             $stmt = $mysqli->prepare('SELECT name FROM shop');
+        require 'db.php';
+        $stmt = $mysqli->prepare('SELECT name FROM shop');
         $stmt->execute();
         $stmt->store_result();
         if($stmt->num_rows()>0){
@@ -15,8 +15,6 @@ class ItemModel{
             }
             return($name);
         }   
-        */
-        $us = getenv('DB_USER');
         return($us);
     }
 }
--- backend/db.php
+++ backend/db.php
@@ -1,12 +1,9 @@
 <?php
-
-/*
 $db = getenv('DB');
 $db_host = getenv('brookematthews.art');
 $db_password = getenv('DB_PASSWORD');
 $db_user = getenv('DB_USER');
 
- $mysqli = new mysqli($db_host,$db_user,$db_password,$db);
+$mysqli = new mysqli($db_host,$db_user,$db_password,$db);
 
-*/
 ?>
