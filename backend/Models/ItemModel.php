f1fa4ea197e6f68ece999a1eb6bbffab7078e4ca
Christopher Kufis <christopherkufis@icloud.com>
2023-04-25 19:47:33 -0700

    test

--- backend/Models/ItemModel.php
+++ backend/Models/ItemModel.php
@@ -4,8 +4,8 @@ namespace Models;
 class ItemModel{
     function get_items(){
         require_once 'db.php';
-        
-        $stmt = $mysqli->prepare('SELECT name FROM shop');
+        /*
+             $stmt = $mysqli->prepare('SELECT name FROM shop');
         $stmt->execute();
         $stmt->store_result();
         if($stmt->num_rows()>0){
@@ -15,7 +15,9 @@ class ItemModel{
                 array_push($results,$name);
             }
             return($name);
-        }
+        }   
+        */
+        echo $db_user;
     }
 }
 
