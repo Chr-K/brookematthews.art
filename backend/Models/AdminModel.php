97d72672db6800814f0723be45af1fbb5ae9d040
Christopher Kufis <christopherkufis@icloud.com>
2023-04-27 18:33:30 -0700

    test

--- backend/Models/AdminModel.php
+++ backend/Models/AdminModel.php
@@ -4,7 +4,22 @@ namespace Models;
 class AdminModel{
     function Login($email,$password){
         require 'db.php';
-        $stmt = $mysqli->prepare('SELECT email, password from shop');
+        $stmt = $mysqli->prepare('SELECT * from shop where email = ?');
+        $stmt->bind_param('s',$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows>0){
+            $row = $result->fetch_assoc();
+            if(password_verify($password,$row['password'])){
+                return($row);
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return('email not found');
+        }
     }
 }
 
