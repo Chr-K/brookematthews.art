94e9a68dc69cb3101d5e4a28788ca9819d369f41
Christopher Kufis <christopherkufis@icloud.com>
2023-04-27 18:29:07 -0700

    test

--- backend/Controllers/AdminController.php
+++ backend/Controllers/AdminController.php
@@ -3,10 +3,7 @@ namespace Controllers;
 
 use Models\AdminModel;
  class AdminController{
-    public function __construct()
-    {
-        echo 'Admin Controller loaded successfull';
-    }
+#test
     function admin_login(){
         $model = new AdminModel();
         $response = $model->Login($_POST['email'],$_POST['password']);
