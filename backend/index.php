12866228255e12d73d5cafce571718271414981c
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 09:44:33 -0700

    test

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,10 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 header('Access-Control-Allow-Origin: https://brookematthews.art');
-header('Access-Control-Allow-Origin: https://admin.brookematthews.art');
-
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header('Access-Control-Allow-Headers: Content-Type');
+
+
 require_once './router.php';
 use Controllers\ItemController;
 use Controllers\AdminController;
