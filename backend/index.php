7c689b7c38f742519628738fe4275a45af8f01c9
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 05:35:16 -0700

    testing

--- backend/index.php
+++ backend/index.php
@@ -7,6 +7,6 @@ use Controllers\AdminController;
 session_start();
 $router = new Router();
 $router->addRoute('GET','/getitems',new ItemController,'get_items');
-$router->addRoute('POST','/adminlogin',new AdminController,'admin_login');
+$router->addRoute('POST','/adminlogin','AdminController','admin_login');
 $router->handleRequest();
 ?>
