f304c16f2f3d282c323bd76277ab60c3a7f038a9
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 06:41:53 -0700

    test

--- backend/Controllers/AdminController.php
+++ backend/Controllers/AdminController.php
@@ -6,7 +6,9 @@ use Models\AdminModel;
 #test
     function admin_login(){
         $model = new AdminModel();
-        $email = json_decode($_POST['email']);
+        $email = $_POST['email'];
+        $emaildata = json_decode($email);
+        echo $emaildata;
         $password = filter_var(json_decode($_POST['password']),FILTER_UNSAFE_RAW);
         $response = $model->Login($email,$password);
         return($response);
