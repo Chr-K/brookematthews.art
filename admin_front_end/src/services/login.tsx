b2be0f2ee449ccf66dc7ed7a185f5112139b1f0c
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 10:29:40 -0700

    test

--- admin_front_end/src/services/login.tsx
+++ admin_front_end/src/services/login.tsx
@@ -6,13 +6,6 @@ export async function AdminLogin(email:string,password:string){
 
     await fetch('https://api.brookematthews.art/adminlogin',{
     method:'POST',
-    headers:{
-        'Content-Type':'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '86400'
-    },
     body: JSON.stringify(data)
     })
     .then(response=>response.json())
