eef520b8e3117f0116cdde7fceafd982346258c0
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 07:00:44 -0700

    test

--- backend/Models/AdminModel.php
+++ backend/Models/AdminModel.php
@@ -14,7 +14,7 @@ class AdminModel{
                 return('success');
             }
             else{
-                return (password_verify(1234,"$2y$10$OMNttpBtP.jGJDlJVg5o6.jpC7SyDu1TFkqef2GPMP3REn1eAflJi\n") . $row['password']);
+                return (password_verify(1234,"$2y$10$OMNttpBtP.jGJDlJVg5o6.jpC7SyDu1TFkqef2GPMP3REn1eAflJi\n"));
             }
         }
         else{
