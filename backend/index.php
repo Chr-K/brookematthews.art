1f326d489d49ad7a3beb1fcf494eac2f83a5a21a
Christopher Kufis <christopherkufis@icloud.com>
2023-04-28 17:20:49 -0700

    [A[A[A[A[A[B[B

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once './router.php';
+require_once __DIR__ . '/router.php';
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
