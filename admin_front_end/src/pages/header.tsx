90ec84831a5a1b1a17b8226f832d518246f3b50f
Christopher Kufis <christopherkufis@icloud.com>
2023-05-02 07:46:43 -0700

    test

--- admin_front_end/src/pages/header.tsx
+++ admin_front_end/src/pages/header.tsx
@@ -4,8 +4,9 @@ import { UserContext } from "../globalContext/user"
 export default function Header(){
 const userContext = useContext(UserContext)
 const user = userContext.user
-function test(){
-    console.log(user?.logged_in)
+async function test(){
+    var res = await user?.logged_in
+    console.log(res)
 }
 
     return(
