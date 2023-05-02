23543dd2057ba46ad501876da5eafe3b5cb5b87a
Christopher Kufis <christopherkufis@icloud.com>
2023-05-02 12:28:32 -0700

    test

--- admin_front_end/src/globalContext/user.tsx
+++ admin_front_end/src/globalContext/user.tsx
@@ -5,20 +5,18 @@ interface UserContextInterface{
     setUser: React.Dispatch<React.SetStateAction<User | null>>
 }
 export interface User{
-    logged_in:boolean
+    logged_in:Boolean
 }
 interface UserContextProviderProps{
     children:React.ReactNode
 }
-interface UserStatus{
-    status:Boolean
-}
+
 async function isLoggedIn(){
         const response = await fetch('https://api.brookematthews.art/get_admin_status',{
         credentials: 'include'
         })
         const data = await response.json()
-        const status:UserStatus = data.status
+        const status = data.status
         return(status)
 }
 
@@ -29,8 +27,12 @@ export const UserContext = createContext<UserContextInterface>({
 export const UserContextProvider: React.FC<UserContextProviderProps> = ({children})=>{
     const [user,setUser] = useState<User | null>({logged_in:false})
     useEffect(()=>{
-        isLoggedIn()
-
+        async function checkStatus(){
+            const state = await isLoggedIn()
+            setUser({logged_in:state})
+        }
+        checkStatus()
+        console.log(user?.logged_in)
         },[])
 return(
     <UserContext.Provider value={{user,setUser}}>
