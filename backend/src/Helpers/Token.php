3ba9440945c4604e15d360fba42527f1786d0f1b
Christopher Kufis <christopherkufis@icloud.com>
2023-05-02 10:49:28 -0700

    test

--- backend/src/Helpers/Token.php
+++ backend/src/Helpers/Token.php
@@ -10,14 +10,17 @@ class Token{
     {
         $this->key = 'test';
     }
-    function MakeToken($id,$email){
+    function MakeToken($id,$email,$sub){
+        if (isset($sub) == false){
+            $sub = '';
+        }
         $time = time();
         $expire = $time + (7 * 24 * 60 * 60);
         $jwt = JWT::encode([
             'id'=>$id,
             'email'=>password_hash($email,PASSWORD_DEFAULT)
         ],$this->key,'HS256');
-        setcookie('jwt',$jwt,$expire,'/','.brookematthews.art',true,true);
+        setcookie('jwt',$jwt,$expire,'/','https:'.$sub.'brookematthews.art',true,true);
     }
     function DecodeToken(){
         $jwt = $_COOKIE['jwt'];
