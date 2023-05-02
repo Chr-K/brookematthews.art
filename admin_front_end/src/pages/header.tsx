d7b42132aa8bf584e5599eef7aa7eb27a3a4b744
Christopher Kufis <christopherkufis@icloud.com>
2023-05-02 09:28:06 -0700

    test

--- admin_front_end/src/pages/header.tsx
+++ admin_front_end/src/pages/header.tsx
@@ -1,11 +1,6 @@
 import { Link } from "react-router-dom"
-import { useContext } from "react"
-import { UserContext } from "../globalContext/user"
 export default function Header(){
-const userContext = useContext(UserContext)
-const user = userContext.user
 function test(){
-    var res = user?.logged_in
 }
 
     return(
