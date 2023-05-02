b7bf30478e6fcda903ac24dba48abc159ba30168
Christopher Kufis <christopherkufis@icloud.com>
2023-05-02 07:52:47 -0700

    test

--- admin_front_end/src/pages/header.tsx
+++ admin_front_end/src/pages/header.tsx
@@ -6,8 +6,7 @@ const userContext = useContext(UserContext)
 const user = userContext.user
 async function test(){
     var res = await user?.logged_in
-    var res_json = res?.json()
-    console.log(res_json)
+    console.log(res)
 }
 
     return(
