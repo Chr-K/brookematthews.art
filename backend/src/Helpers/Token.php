e16aae27eda63f5f74a202ba901ef8ccd7e17d12
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 15:14:51 -0700

    test

--- backend/src/Helpers/Token.php
+++ backend/src/Helpers/Token.php
@@ -25,6 +25,7 @@ class Token{
         $jwt = $_COOKIE['jwt'];
         try{
             $decoded = JWT::decode($jwt,$this->key,array('HS256'));
+            echo $decoded;
             return($decoded);
         }
         catch(Exception $e){
