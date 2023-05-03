57ecd31aba34a08116758ef537ad6b38ce9b1cdb
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 11:09:46 -0700

    test

--- admin_front_end/src/App.tsx
+++ admin_front_end/src/App.tsx
@@ -9,21 +9,8 @@ import { useContext } from 'react'
 import { UserContext } from './globalContext/user'
 function App() {
   const user = useContext(UserContext).user
-  const [debouncedUser,setDebouncedUser] = useState(user)
-  useEffect(()=>{
-    const timerId = setTimeout(()=>{
-      setDebouncedUser(user)
-    },500)
-    return ()=>{
-      clearTimeout(timerId)
-    }
-  },[user])
-  useEffect(()=>{
-    if(debouncedUser?.logged_in == false && window.location.href != 'https://admin.brookematthews.art/'){
-      window.location.href = 'https://admin.brookematthews.art/'
-    }
-    console.log(user?.logged_in)
-  },[debouncedUser])
+
+
   return (
     <div>
     <Header></Header>
--- admin_front_end/src/globalContext/user.tsx
+++ admin_front_end/src/globalContext/user.tsx
@@ -26,6 +26,9 @@ export const UserContextProvider: React.FC<UserContextProviderProps> = ({childre
             })
             const data = await response.json()
             const status = await data.status
+            if(status == false && window.location.href !== 'https://admin.brookematthews.art/'){
+                window.location.href = 'https://admin.brookematthews.art/'
+            }
             setUser({logged_in:await status})
 
     }
