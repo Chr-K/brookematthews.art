dbb2620b0fd03fb8684da8ed66f4db6a48ca8bc4
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 06:19:24 -0700

    test

--- admin_front_end/src/pages/home.tsx
+++ admin_front_end/src/pages/home.tsx
@@ -6,7 +6,7 @@ import { UserContext } from '../globalContext/user'
 export default function Home(){
     const user = useContext(UserContext).user
     useEffect(()=>{
-        if(!user?.logged_in){
+        if(user?.logged_in == false){
             window.location.href = 'https://admin.brookematthews.art'
         }
     },[user])
