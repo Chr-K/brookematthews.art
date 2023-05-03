f731f1a157f22f8312e4dca95e42bd5bd09fdef1
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 11:04:05 -0700

    test

--- admin_front_end/src/App.tsx
+++ admin_front_end/src/App.tsx
@@ -4,17 +4,26 @@ import './styles/text.css'
 import './styles/inputs.css'
 import './styles/buttons.css'
 import Header from './pages/header'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { UserContext } from './globalContext/user'
 function App() {
   const user = useContext(UserContext).user
+  const [debouncedUser,setDebouncedUser] = useState(user)
   useEffect(()=>{
-    if(user?.logged_in == false && window.location.href != 'https://admin.brookematthews.art/'){
+    const timerId = setTimeout(()=>{
+      setDebouncedUser(user)
+    },500)
+    return ()=>{
+      clearTimeout(timerId)
+    }
+  },[user])
+  useEffect(()=>{
+    if(debouncedUser?.logged_in == false && window.location.href != 'https://admin.brookematthews.art/'){
       window.location.href = 'https://admin.brookematthews.art/'
     }
     console.log(user?.logged_in)
-  },[user?.logged_in])
+  },[debouncedUser])
   return (
     <div>
     <Header></Header>
