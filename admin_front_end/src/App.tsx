4e57836d3cb899bb2b013891a46b8417f7862c16
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 10:54:08 -0700

    test

--- admin_front_end/src/App.tsx
+++ admin_front_end/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       window.location.href = 'https://admin.brookematthews.art/'
     }
     console.log(user?.logged_in)
-  },[user])
+  },[])
   return (
     <div>
     <Header></Header>
