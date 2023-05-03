795989f91e6a316ad8bd3cfb14447dec499e8b58
Christopher Kufis <christopherkufis@icloud.com>
2023-05-03 16:37:48 -0700

    test

--- backend/src/Helpers/Token.php
+++ backend/src/Helpers/Token.php
@@ -3,7 +3,6 @@ namespace App\Helpers;
 
 use Exception;
 use Firebase\JWT\JWT;
-
 class Token{
     private $key;
     function __construct()
@@ -20,12 +19,18 @@ class Token{
     }
     function DecodeToken(){
         $jwt = $_COOKIE['jwt'];
-        try{
-            $decoded = JWT::decode($jwt,$this->key,array('HS256'),'test');
-            return $decoded;
-        }
-        catch(Exception $e){
-            echo 'Invalid JWT' . $e->getMessage();
+        list($headerB64, $payloadB64, $signatureB64) = explode('.', $jwt);
+        $headerJson = JWT::urlsafeB64Decode($headerB64);
+        $header = json_decode($headerJson, true);
+        if (isset($header['kid']) && $header['kid'] === 'test') {
+            try {
+                $decoded = JWT::decode($jwt, $this->key, array('HS256'));
+                return $decoded;
+            } catch (Exception $e) {
+                echo 'Invalid JWT: ' . $e->getMessage();
+            }
+        } else {
+            echo 'Invalid or missing "kid" in JWT header';
         }
     }
 }
