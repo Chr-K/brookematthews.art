4e1aff40e44951808f710c98ac8bb7f32483c056
Christopher Kufis <christopherkufis@icloud.com>
2023-05-04 09:17:52 -0700

    test

--- backend/src/Helpers/Token.php
+++ backend/src/Helpers/Token.php
@@ -4,24 +4,14 @@ namespace App\Helpers;
 use Exception;
 use Firebase\JWT\JWT;
 class Token{
-    private $key;
-    function __construct()
-    {
-        $this->key = getenv('PRIVATE_KEY');
-    }
     function MakeToken($id){
+        $privkey = getenv('PRIVATE_KEY');
         $time = time();
         $expire = $time + (7 * 24 * 60 * 60);
-        echo $this->key;
-        /*
-         
         $jwt = JWT::encode([
-        'id'=>$id,
-        ],$this->key,'RS256');
-                setcookie('jwt',$jwt,$expire,'/','.brookematthews.art',true,true);
-
-         */
-        
+            'id'=>$id,
+        ],$privkey,'RS256');
+        setcookie('jwt',$jwt,$expire,'/','.brookematthews.art',true,true);
     }
     function DecodeToken(){
         $jwt = $_COOKIE['jwt'];
