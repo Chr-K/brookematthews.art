7cfa915694cd644bdb1a01da7a89cfe05f54de8d
Christopher Kufis <christopherkufis@icloud.com>
2023-05-04 13:43:33 -0700

    test

--- admin_front_end/src/pages/header.tsx
+++ admin_front_end/src/pages/header.tsx
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom"
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../globalContext/user"
 export default function Header(){
+    const user = useContext(UserContext).user?.logged_in
+    const [headerLink,setHeaderLink] = useState('')
+    useEffect(()=>{
+        if(user){
+            setHeaderLink('/home')
+        }
+        else{
+            setHeaderLink('')
+        }
+    },[user])
     return(
 <div className='header title'>
-<Link className='link' to={''}>Admin Panel</Link>
+<Link className='link' to={headerLink}>Admin Panel</Link>
 </div>
         )
 }
