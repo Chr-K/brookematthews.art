ad65338da04842ee4f9223e99a77640d5673c003
Christopher Kufis <christopherkufis@icloud.com>
2023-05-07 07:49:37 -0700

    test

--- admin_front_end/src/pages/edit_shop.tsx
+++ admin_front_end/src/pages/edit_shop.tsx
@@ -3,15 +3,22 @@ import Item from '../components/item'
 import GetItems from '../services/getitems'
 export default function EditShop(){
         async function getitems_handler(){
+                interface Painting{
+                url:string,
+                name:string,
+                description:string,
+                price:number
+                }
                 const data = await GetItems()
-                console.log (data[0].url)
+                const components = data.map((element:Array<Painting>,index:number)=>{
+                        console.log(element[index].name)
+                })
+                return(components)
         }
     return(
 <div className='edit_shop_container'>
         <button onClick={()=>{getitems_handler()}}>getitems</button>
-        <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
-        <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
-        <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
+
 </div>
         )
 }
