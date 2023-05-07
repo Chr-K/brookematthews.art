673574e7826d7141ad9f7679c84117b8a3a03f6b
Christopher Kufis <christopherkufis@icloud.com>
2023-05-07 07:14:39 -0700

    test

--- admin_front_end/src/pages/edit_shop.tsx
+++ admin_front_end/src/pages/edit_shop.tsx
@@ -2,9 +2,9 @@ import './styles/edit_shop.css'
 import Item from '../components/item'
 import GetItems from '../services/getitems'
 export default function EditShop(){
-        GetItems()
     return(
 <div className='edit_shop_container'>
+        <button onClick={()=>{GetItems()}}>getitems</button>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
--- admin_front_end/src/services/getitems.tsx
+++ admin_front_end/src/services/getitems.tsx
@@ -1,4 +1,5 @@
 export default async function GetItems(){
 const response = await fetch('https://api.brookematthews.art/getitems')
+console.log(response)
 return response
 }
