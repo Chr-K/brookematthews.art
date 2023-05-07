ac07d74bbfe91bda9bf2008628f4c3b183f560ff
Christopher Kufis <christopherkufis@icloud.com>
2023-05-07 07:34:16 -0700

    test

--- admin_front_end/src/pages/edit_shop.tsx
+++ admin_front_end/src/pages/edit_shop.tsx
@@ -2,9 +2,13 @@ import './styles/edit_shop.css'
 import Item from '../components/item'
 import GetItems from '../services/getitems'
 export default function EditShop(){
+        async function getitems_handler(){
+                const data = await GetItems()
+                console.log (data[0].url)
+        }
     return(
 <div className='edit_shop_container'>
-        <button onClick={()=>{GetItems()}}>getitems</button>
+        <button onClick={()=>{getitems_handler()}}>getitems</button>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
         <Item title={'title'} price={12} imgurl={'./public/painting.jpeg'}></Item>
