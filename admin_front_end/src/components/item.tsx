4c54e0b7264d5079619be73a246a7185e645e3c7
Christopher Kufis <christopherkufis@icloud.com>
2023-05-09 08:42:27 -0700

    test

--- admin_front_end/src/components/item.tsx
+++ admin_front_end/src/components/item.tsx
@@ -7,10 +7,11 @@ import { changePicture } from '../services/changePicture.tsx'
 interface Item{
     title:string,
     imgurl:string,
-    price:number
+    price:number,
+    key:number,
 }
 
-export default function Item({title,imgurl,price}:Item){
+export default function Item({title,imgurl,price,key}:Item){
     const [itemTitle,setItemTitle] = useState(title)
     const [itemPrice,setItemPrice] = useState(price)
     //checkmark state for inputs according to their type
@@ -40,14 +41,15 @@ export default function Item({title,imgurl,price}:Item){
 
     }
     function pictureInput(){
-        document.getElementById('change_picture_input')?.click()
+        document.getElementById(key.toString())?.click()
     }
     function selectPicture(e:React.ChangeEvent<HTMLInputElement>){
         const file = e.currentTarget.files![0]
-        const imgElemenet = document.getElementById(imgurl) as HTMLImageElement
+        const imgElemenet = document.getElementById(title + 'img') as HTMLImageElement
         if(file.type.startsWith('image')){
             imgElemenet.src = URL.createObjectURL(file);
             changePicture(file,imgurl)
+            console.log(imgurl)
         }
         else{
             alert('invalid file type')
@@ -66,11 +68,11 @@ export default function Item({title,imgurl,price}:Item){
                     <CheckMark></CheckMark>
                 </div>
             </div>
-            <img id='itemPicture' className='edit_image' src={imgurl}></img>
+            <img id={title+'img'} className='edit_image' src={imgurl}></img>
             <div className='edit_item_options'>
             <div className='picture-icon-container' onClick={()=>pictureInput()}>
                 <PictureIcon></PictureIcon>
-                <input name='uploadPicture' id={imgurl} onChange={(e)=>selectPicture(e)} type='file' style={{display:'none'}}></input>
+                <input name='uploadPicture' id={key.toString()} onChange={(e)=>selectPicture(e)} type='file' style={{display:'none'}}></input>
             </div>
             <input className='text-primary input-primary white text-center col2' onChange={(e)=>{inputChange(e,setItemPrice,setPriceCheck)}} onBlur={(e)=>{inputBlur(e,setItemPrice,setPriceCheck,price.toString())}} onFocus={(e)=>inputFocus(e,setItemPrice,price.toString())} defaultValue={itemPrice}></input>
             <div style={{display:priceCheck}} onClick={()=>{alert('hello')}} className='price_check'>
