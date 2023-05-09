6367ccdecd12d2aa512c9b15b469ad7b665f79ca
Christopher Kufis <christopherkufis@icloud.com>
2023-05-09 05:52:31 -0700

    test

--- admin_front_end/src/services/changePicture.tsx
+++ admin_front_end/src/services/changePicture.tsx
@@ -1,8 +1,7 @@
 export async function changePicture(file:File,currentUrl:string){
-    const file_info = {currentPhotoUrl:currentUrl}
+    console.log(currentUrl)
     const data = new FormData()
     data.append('file',file,file.name)
-    data.append('data',JSON.stringify(file_info))
     const response = await fetch('https://api.brookematthews.art/updateitemphoto',{
         method:'POST',
         credentials:'include',
