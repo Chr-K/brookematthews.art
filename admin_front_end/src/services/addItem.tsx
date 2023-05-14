28031c65129de61a89d621d4975dc94ba6a17df9
Christopher Kufis <christopherkufis@icloud.com>
2023-05-14 09:18:51 -0700

    test

--- admin_front_end/src/services/addItem.tsx
+++ admin_front_end/src/services/addItem.tsx
@@ -1,14 +1,15 @@
 export async function AddNewItem(name:string,description:string,price:string,file:File){
 const data = new FormData()
+data.append('uploadPicture',file,file.name)
+data.append('itemName',name)
+data.append('description',description)
+data.append('price',price)
 const response = await fetch('https://api.brookematthews.art/additem',{
     method:'POST',
     credentials:'include',
     body:data,
 })
-data.append('uploadPicture',file,file.name)
-data.append('itemName',name)
-data.append('description',description)
-data.append('price',price)
+
 const responseText = await response.text()
 console.log(responseText)
 return(response)
